5a62c077379323a87221918b2f207ae52c44d59c
Hirano-Soura <ghostar2939@gmail.com>
2026-07-22 20:24:52 +0900

    update 2026-07-22

---
+++ status.js
@@ -0,0 +1,19 @@
+window.__portalStatus({
+ "id": "toukei",
+ "date": "2026-07-22",
+ "updated": "2026-07-22",
+ "headline": "指数分布（2級）",
+ "sub": "全50トピック中 12個目",
+ "progress": {
+  "done": 12,
+  "total": 50
+ },
+ "next": "大数の法則",
+ "items": [
+  {
+   "label": "演習",
+   "state": "ready",
+   "detail": "指数分布"
+  }
+ ]
+});
